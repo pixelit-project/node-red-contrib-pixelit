c55ef1270412d4bd6bf4a75d468ae44b14108531
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-04-22 23:36:38 +0200

    Update

--- pixelit/pixelit.js
+++ pixelit/pixelit.js
@@ -15,33 +15,55 @@ module.exports = function (RED) {
         this.on('input', function (msg) {
 
             var sleepModeAktiv = context.get("sleepModeAktiv") || false;
+            var sendOverHTTPAktiv = true;
+            var mqttMasterTopic = getValue(config.masterTopic, msg) || '';
+
+            // Clean Master Topic
+            if (mqttMasterTopic.substr(mqttMasterTopic.length - 1) === "/") {
+                mqttMasterTopic = mqttMasterTopic.slice(0, -1);
+            }
 
             // Check is IP Config?!
             if (config.ip === undefined || config.ip === '') {
-                node.status({ fill: "red", shape: "dot", text: "Please set IP-Address" });
-                return;
+                //node.status({ fill: "red", shape: "dot", text: "Please set IP-Address" });
+                sendOverHTTPAktiv = false;
             }
 
             // This topic sent by all providers to update the data in the context
             if (msg.topic === "screen_data_update") {
                 if (msg.screenName !== undefined && msg.duration !== undefined) {
                     context.set(msg.screenName, msg);
-                    node.status({ fill: "blue", shape: "ring", text: "Screen [" + msg.screenName + "] stored" });
-                }
-                else {
-                    node.status({ fill: "red", shape: "dot", text: "Screen data cannot store!" });
+                    node.status({
+                        fill: "blue",
+                        shape: "ring",
+                        text: "Screen [" + msg.screenName + "] stored"
+                    });
+                } else {
+                    node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: "Screen data cannot store!"
+                    });
                 }
             }
 
             // Check is SleepMode Aktiv?! (Ab hier ist dann schluß! :-) )
             if (sleepModeAktiv == true && msg.sleepMode == null) {
-                node.status({ fill: "yellow", shape: "ring", text: "Sleep Mode Aktiv!" });
+                node.status({
+                    fill: "yellow",
+                    shape: "ring",
+                    text: "Sleep Mode Aktiv!"
+                });
                 return;
             }
 
             // This topic sent by all providers to update the data in the context
             if (msg.topic === "matrix_control") {
-                node.status({ fill: "grey", shape: "ring", text: "Matrix Control send" });
+                node.status({
+                    fill: "grey",
+                    shape: "ring",
+                    text: "Matrix Control send"
+                });
 
                 // SleepMode Steuerung 
                 if (msg.sleepMode !== undefined && msg.sleepMode == true) {
@@ -54,15 +76,18 @@ module.exports = function (RED) {
                     context.set("sleepModeAktiv", sleepModeAktiv);
                     SendToPixelItScreen(CreateScreenJson(msg));
                     GetNextScreen();
-                }
-                else if (sleepModeAktiv == false) {
+                } else if (sleepModeAktiv == false) {
                     SendToPixelItScreen(CreateScreenJson(msg));
                 }
             }
 
             // This topic sent by all providers to update the data in the context
             if (msg.topic === "matrix_config") {
-                node.status({ fill: "grey", shape: "ring", text: "Matrix Config send" });
+                node.status({
+                    fill: "grey",
+                    shape: "ring",
+                    text: "Matrix Config send"
+                });
                 SendToPixelItConfig(CreateConfigJson(msg));
             }
 
@@ -70,13 +95,20 @@ module.exports = function (RED) {
             if (msg.topic === "playlist_update") {
                 if ("undefined" !== typeof (msg.payload[0]["screenName"])) {
                     context.set("playlist", msg.payload);
-                    node.status({ fill: "green", shape: "ring", text: "New Playlist stored" });
+                    node.status({
+                        fill: "green",
+                        shape: "ring",
+                        text: "New Playlist stored"
+                    });
 
                     context.set("nowPlay", 0);
                     GetNextScreen();
-                }
-                else {
-                    node.status({ fill: "red", shape: "dot", text: "New Playlist cannot store!" });
+                } else {
+                    node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: "New Playlist cannot store!"
+                    });
                 }
             }
 
@@ -92,7 +124,11 @@ module.exports = function (RED) {
                 }
 
                 context.set("timeout", setTimeout(GetNextScreen, (msg.duration * 1000)));
-                node.status({ fill: "yellow", shape: "ring", text: status });
+                node.status({
+                    fill: "yellow",
+                    shape: "ring",
+                    text: status
+                });
                 //node.send(CreateJson(msg));
                 SendToPixelItScreen(CreateScreenJson(msg));
             }
@@ -132,7 +168,11 @@ module.exports = function (RED) {
                         msg = screen;
 
 
-                        node.status({ fill: "green", shape: "ring", text: "Displaying [" + play.screenName + "] now" });
+                        node.status({
+                            fill: "green",
+                            shape: "ring",
+                            text: "Displaying [" + play.screenName + "] now"
+                        });
 
                         if (context.get("timeout")) {
                             clearTimeout(context.get("timeout"));
@@ -160,6 +200,31 @@ module.exports = function (RED) {
                 if (msg.clock) {
                     json += '"clock":{"show":' + msg.clock.show + ',"switchAktiv":' + msg.clock.switchAktiv + ', "withSeconds":' + msg.clock.withSeconds + ',"switchSec":' + msg.clock.switchSec + ',"color":{"r":' + msg.clock.color.r + ',"g":' + msg.clock.color.g + ',"b":' + msg.clock.color.b + '}}';
                 }
+                if (msg.sound) {
+                    json += '"sound":{';
+
+                    if (msg.sound.volume) {
+                        json += '"volume":' + parseInt(msg.sound.volume);
+                    }
+
+                    if (msg.sound.control) {
+                        json += ','
+
+                        if (msg.sound.control == "play") {
+                            json += '"control":"' + msg.sound.control + '",';
+
+                            if (msg.sound.folder) {
+                                json += '"folder":' + parseInt(msg.sound.folder) + ',';
+                            }
+
+                            if (msg.sound.file) {
+                                json += '"file":' + parseInt(msg.sound.file) + '},';
+                            }
+                        } else {
+                            json += '"control":"' + msg.sound.control + '"},';
+                        }
+                    }
+                }
                 if (msg.bitmap) {
                     json += '"bitmap":{"data":' + GetBitMap(msg.bitmap.data) + ',"position":{"x":' + msg.bitmap.position.x + ',"y":' + msg.bitmap.position.y + '},"size":{"width":' + msg.bitmap.size.width + ',"height":' + msg.bitmap.size.height + '}},';
                 }
@@ -181,8 +246,7 @@ module.exports = function (RED) {
                     if (msg.text.scrollText) {
                         if ((msg.text.scrollText == "true") || (msg.text.scrollText == "false")) {
                             json += '"scrollText":' + (msg.text.scrollText == "true") + ',';
-                        }
-                        else {
+                        } else {
                             json += '"scrollText":"' + msg.text.scrollText + '",';
                         }
 
@@ -224,40 +288,61 @@ module.exports = function (RED) {
             }
 
             function SendToPixelItConfig(myjson) {
+                var result = {
+                    topic: mqttMasterTopic + '/setConfig',
+                    payload: myjson
+                };
+                node.send(result);
 
-                checkConnection(config.ip).then(function () {
-
-                    request({
-                        uri: 'http://' + config.ip + '/api/config',
-                        method: 'POST',
-                        json: false,
-                        body: myjson
-                    }, function (error, response, body) { });
-
-                }, function (err) {
-                    node.status({ fill: "yellow", shape: "dot", text: "Cannot send.." });
-                });
+                if (sendOverHTTPAktiv) {
+                    checkConnection(config.ip).then(function () {
+                        request({
+                            uri: 'http://' + config.ip + '/api/config',
+                            method: 'POST',
+                            json: false,
+                            body: myjson
+                        }, function (error, response, body) {});
+                    }, function (err) {
+                        node.status({
+                            fill: "yellow",
+                            shape: "dot",
+                            text: "Cannot send.."
+                        });
+                    });
+                }
             }
 
             function SendToPixelItScreen(myjson) {
+                var result = {
+                    topic: mqttMasterTopic + '/setScreen',
+                    payload: myjson
+                };
+                node.send(result);
 
-                checkConnection(config.ip).then(function () {
+                if (sendOverHTTPAktiv) {
+                    checkConnection(config.ip).then(function () {
+                        request({
+                            uri: 'http://' + config.ip + '/api/screen',
+                            method: 'POST',
+                            json: false,
+                            body: myjson
+                        }, function (error, response, body) {});
 
-                    request({
-                        uri: 'http://' + config.ip + '/api/screen',
-                        method: 'POST',
-                        json: false,
-                        body: myjson
-                    }, function (error, response, body) { });
-
-                }, function (err) {
-                    node.status({ fill: "yellow", shape: "dot", text: "Cannot send.." });
-                });
+                    }, function (err) {
+                        node.status({
+                            fill: "yellow",
+                            shape: "dot",
+                            text: "Cannot send.."
+                        });
+                    });
+                }
             }
 
             function sleep(millis) {
                 return new promise(function (resolve, reject) {
-                    setTimeout(function () { resolve(); }, millis);
+                    setTimeout(function () {
+                        resolve();
+                    }, millis);
                 });
             }
 
@@ -280,39 +365,38 @@ module.exports = function (RED) {
                 });
             }
 
-
             function GetBitMap(input) {
-                if (input.includes(",")) {
-                    return input;
-                }
-                else {
-                    input = input.trim();
-                    var bmp = context.get("bmpCache_" + input);
+                var webBmp = "[64512,0,0,0,0,0,0,64512,0,64512,0,0,0,0,64512,0,0,0,64512,0,0,64512,0,0,0,0,0,64512,64512,0,0,0,0,0,0,64512,64512,0,0,0,0,0,64512,0,0,64512,0,0,0,64512,0,0,0,0,64512,0,64512,0,0,0,0,0,0,64512]";
+                if (input != null) {
+                    if (input.includes(",")) {
+                        return input;
+                    } else {
+                        input = input.trim();
+                        var bmp = context.get("bmpCache_" + input);
 
-                    if (bmp != null) {
-                        return bmp;
+                        if (bmp != null) {
+                            return bmp;
+                        } else {
+                            try {
+                                var res = syncRequest('GET', 'https://api.bastelbunker.de/PixelItService/GetBMPByID/' + input, {
+                                    headers: {
+                                        'User-Agent': 'Node_Red_Core'
+                                    },
+                                    timeout: 1000
+                                });
+                                var body = JSON.parse(res.getBody('utf8'));
+                            } catch (error) {
+                                body = null;
+                            }
+
+                            if (body != null && body.id != null && body.id != 0) {
+                                webBmp = body.rgB565Array;
+                                context.set("bmpCache_" + input, webBmp);
+                            }
+                        }
                     }
-                    else {
-                        try {
-                            var res = syncRequest('GET', 'https://api.bastelbunker.de/PixelItService/GetBMPByID/' + input, { headers: { 'User-Agent': 'Node_Red_Core' }, timeout: 1000 });
-                            var body = JSON.parse(res.getBody('utf8'));
-                        } catch (error) {
-                            body = null;
-                        }
-
-
-                        var webBmp;
-                        if (body == null || body.id == null || body.id == 0) {
-                            webBmp = "[64512,0,0,0,0,0,0,64512,0,64512,0,0,0,0,64512,0,0,0,64512,0,0,64512,0,0,0,0,0,64512,64512,0,0,0,0,0,0,64512,64512,0,0,0,0,0,64512,0,0,64512,0,0,0,64512,0,0,0,0,64512,0,64512,0,0,0,0,0,0,64512]";
-                        }
-                        else {
-                            webBmp = body.rgB565Array;
-                            context.set("bmpCache_" + input, webBmp);
-                        }
-
-                        return webBmp;
-                    }
                 }
+                return webBmp;
             }
         });
     }
@@ -473,7 +557,20 @@ module.exports = function (RED) {
         });
     }
 
-
+    // Sound
+    function sound(config) {
+        RED.nodes.createNode(this, config);
+        var context = this.context();
+        var node = this;
+        this.on('input', function (msg) {
+            msg.sound = {};
+            msg.sound.volume = getValue(config.inVolume);
+            msg.sound.control = getValue(config.inControl);
+            msg.sound.folder = getValue(config.inFolder);
+            msg.sound.file = getValue(config.inFile);
+            node.send(msg)
+        });
+    }
 
 
     function getValue(input, msg) {
@@ -481,8 +578,7 @@ module.exports = function (RED) {
         if (input) {
             if (isMsgVal(input)) {
                 output = RED.util.getMessageProperty(msg, cleanMsgVal(input));
-            }
-            else {
+            } else {
                 output = input;
             }
         }
@@ -497,6 +593,7 @@ module.exports = function (RED) {
     function cleanMsgVal(input) {
         return input.trim().replace("{{", "").replace("}}", "").replace("msg.", "");
     }
+
     function cleanScreenName(input) {
         return input.trim().replace(" ", "_");
     }
@@ -516,5 +613,5 @@ module.exports = function (RED) {
     RED.nodes.registerType("Bitmap", bitmap);
     RED.nodes.registerType("Bitmap Animation", bitmapAnimation);
     RED.nodes.registerType("Clock", clock);
+    RED.nodes.registerType("Sound", sound);
 };
-
